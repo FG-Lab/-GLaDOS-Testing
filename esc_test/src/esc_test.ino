931a6e13188e1d7741a07411843fbe58edce4df8
Afeef Neiroukh <s44aneir@uni-bonn.de>
2022-12-31 23:06:52 +0100

    Der ESC-Test wurde stark verbessert und enthällt nun auch eine Kalibrierung built-in. Eine kurze Anleitung gibt es morgen.

--- esc_test/src/esc_test.ino
+++ esc_test/src/esc_test.ino
@@ -1,13 +1,17 @@
 #include <Arduino.h>
 #include <Servo.h>
+#include <EEPROM.h>
 
 Servo ESC;
+int maxSpeed = 0;
 
 void setup() {
   Serial.begin(9600);
   ESC.attach(9, 0, 60);
-  ESC.write(0);
-  delay(2000);
+  pinMode(2, INPUT);
+  pinMode(13, OUTPUT);
+
+  maxSpeed = EEPROM.read(0);
 }
 
 
@@ -15,14 +19,51 @@ double currentSpeed = 0;
 double newSpeed;
 
 void loop() {
-  newSpeed = map(analogRead(0), 0, 1023, 0, 60);
+  if(digitalRead(2)){
+    maxSpeed = esc_calibration();
+  } 
+
+  newSpeed = map(analogRead(0), 0, 1023, 0, maxSpeed);
   if(newSpeed - currentSpeed > 3){
     Serial.println("Jump too big!");
-    currentSpeed = currentSpeed + 0.6;
+    currentSpeed += 0.5;
   } else{
-    currentSpeed = newSpeed;
+      if(currentSpeed - newSpeed > 3){
+        Serial.println("Jump too big!");
+        currentSpeed -= 0.5;
+      } else{
+        currentSpeed = newSpeed;
+      }
   }
+ 
   ESC.write(currentSpeed);
-  Serial.print((currentSpeed / 60) * 100);
+  Serial.print((currentSpeed / 180) * 100);
   Serial.println("%");
+}
+
+int esc_calibration(){
+  // Test-sequence SETUP
+  int maxSpeed = 0;
+  long time = millis();
+  delay(1000);
+
+  // Test-sequence START
+  digitalWrite(13, HIGH);
+  while(!digitalRead(2) & maxSpeed < 180){
+    ESC.write(maxSpeed);
+    if(millis() - time > 250){
+      maxSpeed += 1;
+      Serial.println(maxSpeed);
+      time = millis();
+    }
+  }
+
+  // Test-sequence SHUTDOWN
+  digitalWrite(13, LOW);
+  delay(2500);
+  if(maxSpeed-4 >= 0){
+    EEPROM.write(0, maxSpeed-4);
+    return maxSpeed-4;
+  }
+  return maxSpeed;
 }
